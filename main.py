2568852daacd45491d7a044a2756fbbff6c6d730
Nelson Malbone <nhmalbone@icloud.com>
2024-12-16 19:01:47 -0500

    place a header to get more content type like the date in what kind of application it is

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import requests
 
+
 try: # to make sure website is valid
 
     re = requests.get('https://www.github.com')
     status_code = re.status_code
     response_time = re.elapsed
+    header_date = re.headers['date']
     # checking status of 200 or not
     if status_code == 200:
         print('online')
     else:
         print('offline')
-
     print(status_code)
     print(response_time.microseconds, 'microseconds')
+    print(header_date)
 
 except:
     print('n/a')
+    print('offline')

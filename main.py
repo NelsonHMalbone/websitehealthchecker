d526e469c9803aeb0dff98a393809eb4c89165db
Nelson Malbone <nhmalbone@icloud.com>
2024-12-18 17:54:23 -0500

    runs thur the first two with no issues gets to the 404 error in just breaks

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import requests
 
 "input"
+def send_requests_to_websites(filepath):
+    with open(filepath, 'r') as file:
+        try:
+            for line in file:
+                url = line.strip()  # removes any whitespaces
+                response = requests.get(url)
+                response.raise_for_status()
+                print(f'response for {url}: {response.status_code}')
+        except:
+
+            print(f'An error occured:')
+
+
 filepath = 'websites.txt'
-
-
-
-"results"
-print(f'')
+send_requests_to_websites(filepath)

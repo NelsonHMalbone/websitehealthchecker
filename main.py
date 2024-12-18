ad6346aed292afaa6d5f6265d0babb896dfa516d
Nelson Malbone <nhmalbone@icloud.com>
2024-12-18 16:12:13 -0500

    restarted the whole project

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 import requests
 
+"input"
+filepath = 'websites.txt'
 
-try: # to make sure website is valid
-    url = 'https://www.google.com'
-    re = requests.get(url)
-    status_code = re.status_code
-    response_time = re.elapsed
-    header_date = re.headers['date']
-    # checking status of 200 or not
-    status = 'Online' if status_code == 200 else 'Offline'
-    print(status)
-    print(status_code)
-    print(response_time.microseconds, 'ms')
-    print(header_date)
 
-except:
-    print('n/a')
-    print('offline')
+
+"results"
+print(f'')
